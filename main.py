4bb4de23ff385f4d79a8d178ef3e51185c534b8f
F0xhopper <you@example.com>
2024-04-11 17:20:45 +0100

    most of comments added

--- main.py
+++ main.py
@@ -1,147 +1,179 @@
 import tkinter as tk
-from tkinter import messagebox
 import random
-import math
 import time
 import threading
+
 class TypingTestApp(tk.Tk):
     def __init__(self):
         super().__init__()
+        # Initialize the tkinter application
         self.title("Typing Test")
-        self.config(padx=20,pady=20,background='black')
-        self.paragraphs = ["The sun dipped below the horizon, casting long shadows across the \nlandscape. The sky turned a vibrant orange, painting a breathtaking scene for\n those who cared to look. Nature's beauty never failed to inspire awe.","High in the mountains, the air was crisp and thin, carrying the scent of \npine and snow. The silence was profound, broken only bythe occasional chirp\n of a bird or the rustle of leaves in the breeze. It was a place of tranquility,\n far removed from the noise of the world below."]
+        self.config(padx=30, pady=30, background='black')
+        # Define paragraphs to be copied
+        self.paragraphs = ["The sun dipped below the horizon, casting long shadows across the \nlandscape. The sky turned a vibrant orange, painting a breathtaking scene for\n those who cared to look. Nature's beauty never failed to inspire awe.",
+                           "High in the mountains, the air was crisp and thin, carrying the scent of \npine and snow. The silence was profound, broken only by the occasional chirp\n of a bird or the rustle of leaves in the breeze. It was a place of tranquility,\n far removed from the noise of the world below."]
+        # Initialize variables
         self.current_paragraph = random.choice(self.paragraphs)
         self.running = False
         self.username = ""
-        self.get_username()
         self.counter = 0
-        self.highscore = 0 
-        self.highscore_username = ''
         self.total_letters_types = 0
         self.total_mistakes = 0
+        # Make username input pop up appear
+        self.get_username()
+        # Create GUI widgets
         self.create_widgets()
+        # Load highscores from file
         self.load_highscore()
 
     def create_widgets(self):
-        self.title = tk.Label(self, justify="left", text="TypeTest.io", background='black', fg='white', font=("Helvetica", 35))     
+        # Create title label
+        self.title = tk.Label(self, justify="left", text="TypeTest.io", background='black', fg='white', font=("Helvetica", 35))
         self.title.grid(column=1, row=0)
-
-        self.highscore_label = tk.Label(self, justify="left", anchor="w", text=f'Highest: {self.highscore_username} {self.highscore:.0f} w/m', wraplength=200, background='black', fg='white', font=("Helvetica", 14))     
+        # Create label for highscores
+        self.highscore_label = tk.Label(self, justify="left", anchor="w", wraplength=200, background='black', fg='white', font=("Helvetica", 14))
         self.highscore_label.grid(column=0, row=0, columnspan=1)
-          
-        self.name_label = tk.Label(self, width=16, text=f'User playing:{self.username}', background='black', fg='white', font=("Helvetica", 14))     
+        # Create label for displaying username
+        self.name_label = tk.Label(self, width=16, background='black', fg='white', font=("Helvetica", 14))
         self.name_label.grid(column=2, row=0, columnspan=1)
-
+        # Create frame for statistics
         self.stats_frame = tk.Frame(background='black')
         self.stats_frame.grid(column=1, row=1, pady=10)
-      
+        # Create labels for timer, accuracy, and words per minute (WPM)
         self.timer = tk.Label(self.stats_frame, text="0 cpm", borderwidth=3, relief="solid", fg='white', highlightcolor='black', background='black', font=("Helvetica", 20))
         self.timer.grid(column=0, row=0, pady=2, ipadx=20, ipady=0)
-      
         self.accuracy_label = tk.Label(self.stats_frame, text="0% Accuracy", borderwidth=3, relief="solid", fg='white', highlightcolor='black', background='black', font=("Helvetica", 20))
         self.accuracy_label.grid(column=0, row=2, pady=2, ipadx=20, ipady=0)
-
         self.wpm_label = tk.Label(self.stats_frame, text="0 wpm", borderwidth=3, relief="solid", fg='white', highlightcolor='black', background='black', font=("Helvetica", 20))
         self.wpm_label.grid(column=0, row=1, pady=2, ipadx=20, ipady=0)
-
+        # Create label for displaying paragraph to type
         self.paragraph = tk.Label(self, text=self.current_paragraph, fg='white', background='black', font=("Helvetica", 17), width=60, height=4)
         self.paragraph.grid(row=2, column=0, columnspan=3, pady=10)
-
-        self.text_input = tk.Text(height=8, background='white', fg='black', font=(24))      
+        # Create text input widget for typing
+        self.text_input = tk.Text(height=8, background='white', fg='black', font=(24))
         self.text_input.grid(row=3, column=0, columnspan=3, pady=(10))
         self.text_input.bind("<KeyRelease>", self.start)
-       
+        # Create button for restarting the typing test
         self.restart = tk.Button(text='Restart', highlightbackground="black", highlightthickness=1, background='black', command=self.reset)
         self.restart.config(height=2, width=6)
         self.restart.grid(row=4, column=1, pady=10)
-    
+
+    # Method for getting the users username
     def get_username(self):
+        # Initialize the popup
         username_window = tk.Toplevel(self)
         username_window.title("Enter Username")
         username_window.config(padx=20, pady=20)
+        # Create label for instructions
         username_label = tk.Label(username_window, text="Enter your username:", font=("Helvetica", 14))
         username_label.pack()
+        # Create input for username
         self.username_entry = tk.Entry(username_window)
         self.username_entry.pack()
+        # Set entry in focus
         self.username_entry.focus_set()
+        # Create submit button
         submit_button = tk.Button(username_window, text="Submit", command=self.save_username)
         submit_button.pack()
 
+    # Method for saving the entered username
     def save_username(self):
+        # Set username vairable to the one entered by user
         self.username = self.username_entry.get()
+        # Checkts if the input is empty, destroys popup, clears input and initiates typing test if not
         if self.username.strip() != "":
-  
- 
-            self.name_label.config(text=f'Playing:{self.username}')
+            self.name_label.config(text=f'Playing: {self.username}')
             self.username_entry.delete(0, tk.END)
             self.username_entry.master.destroy()
-    def start(self,event):
-        self.total_letters_types +=1 
+
+    # Method triggered when typing to calculate if paragraph is matching or wrong
+    def start(self, event):
+        # Count the amount typed
+        self.total_letters_types += 1
+        # If not already running start keeping count of scores and time
         if not self.running:
             self.running = True
-            t = threading.Thread(target=self.update_times_scores)
+            t = threading.Thread(target=self.update_time_scores)
             t.start()
-        if not self.current_paragraph.replace("\n", "").startswith(self.text_input.get("1.0","end-1c")):
+        # If input doesn't match paragraph makes input text red and adds a mistake onto mistake counter
+        if not self.current_paragraph.replace("\n", "").startswith(self.text_input.get("1.0", "end-1c")):
             self.text_input.config(fg='red')
             self.total_mistakes += 1
-            
         else:
             self.text_input.config(fg='black')
-        if self.current_paragraph.replace("\n", "") == self.text_input.get("1.0","end-1c"):
-             self.running = False
-             self.text_input.config(fg='green')
-             if self.wpm > self.highscore:
-                self.set_new_highscore()
-                self.load_highscore()
+        # When the paragraph is completely typed end session, save score to file and turn text green
+        if self.current_paragraph.replace("\n", "") == self.text_input.get("1.0", "end-1c"):
+            self.running = False
+            self.text_input.config(fg='green')
+            self.set_new_score()
+            self.load_highscore()
 
-    def update_times_scores(self):
+    # Method for updating time and scores
+    def update_time_scores(self):
         while self.running:
+            # Calculate every 0.1 seconds
             time.sleep(0.1)
             self.counter += 0.1 
+            # Initialize accuracy variable
             accuracy = 100
-            characters = len(''.join(self.text_input.get("1.0","end-1c").split(' ')))
+            # Initialize total words/characters variables
+            total_characters = len(''.join(self.text_input.get("1.0","end-1c").split(' ')))
+            total_words = len(self.text_input.get("1.0","end-1c").split(' '))
+            # When a mistake is made calculate the new accuracy
             if self.total_mistakes != 0:
                 accuracy = int((self.total_letters_types - self.total_mistakes) / self.total_letters_types *100 )
-            words = len(self.text_input.get("1.0","end-1c").split(' '))
-            self.wpm = words // (self.counter / 60)
-            cpm = characters // (self.counter / 60)
+            # When the times is started calculate new wpm and cpm
+            if self.counter != 0:  
+                self.wpm = total_words // (self.counter / 60.0)
+                cpm = total_characters // (self.counter / 60.0)
+            else:
+                self.wpm = 0
+                cpm = 0
+            # Update the accuracy, timer and wpm labels
             self.accuracy_label.config(text=f'{accuracy:.0f}% Accuracy')
             self.timer.config(text=f'{cpm:.1f} cpm')
             self.wpm_label.config(text=f'{self.wpm:.0f} wpm')
 
+    # Method loading the highest scores from the txt file
     def load_highscore(self):
       with open("highscore.txt", "r") as score_file:
         lines = score_file.readlines()
-        self.highscores = []
+        # Initialize scores variable
+        scores = []
+        # read and add scores to the scores array as tuple
         for line in lines:
             parts = line.strip().split(":")
             username = parts[0]
             wpm = float(parts[1])
-            self.highscores.append((username, wpm))
-        # Sort highscores based on WPM, highest first
-        self.highscores.sort(key=lambda x: x[1], reverse=True)
-        # Display the top 3 scores
+            scores.append((username, wpm))
+        # Sorts scores into order based on wpm
+        scores.sort(key=lambda x: x[1], reverse=True)
+        # Initialise top_3_scores_label_text variable and read, format and append the top 3 scores to top_3_scores_label_text from scores array
         top_3_scores_label_text = []
-        for i, (username, wpm) in enumerate(self.highscores[:3]):
+        for i, (username, wpm) in enumerate(scores[:3]):
             top_3_scores_label_text.append(f'{i+1}. {username}: {wpm:.0f} wpm')
+        # Update highscres label
         self.highscore_label.config(text='\n'.join(top_3_scores_label_text))
-            
-    def set_new_highscore(self):
+        
+    # Method for writing new score to the txt file        
+    def set_new_score(self):
         with open("highscore.txt", "a") as score_file:
             score_file.write(f'{self.username}:{self.wpm}\n')
     
-        
-    def reset(self):
+    # Method for restarting the test    
+    def reset(self):   
+        time.sleep(0.1)
+        self.text_input.delete("1.0","end-1c") 
         self.running = False
-        self.counter = 0
+        self.counter = 0.0
         self.total_letters_types = 0
         self.total_mistakes = 0
-        self.text_input.delete("1.0","end-1c") 
-        self.timer.config(text=f'{0} s')
-        self.wpm_label.config(text=f'{0} w/m')
+        self.wpm = 0
+        self.timer.config(text='0 cpm')
+        self.wpm_label.config(text='0 w/m')
         self.current_paragraph = random.choice(self.paragraphs)
-        self.paragraph.config(text=f'{self.current_paragraph}')
-        self.accuracy_label.config(text=f'0% Accuracy')
+        self.paragraph.config(text=self.current_paragraph)
+        self.accuracy_label.config(text='0% Accuracy')
 
 
 
